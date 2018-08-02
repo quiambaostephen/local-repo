6f4553858a31d810fc7ba26d6f8c7a94e428dca6
Stephen Quiambao <quiambaostephen@gmail.com>
2018-08-02 21:12:56 +0800

    Added license

--- perfect_cube.py
+++ perfect_cube.py
@@ -3,11 +3,11 @@
 x = int(input('Enter an integer: '))
 
 for ans in range(0, abs(x) + 1):
-    if ans1 ** 3 == abs(x):
+    if ans ** 3 == abs(x):
         break
-if ans1 ** 3 != abs(x):
+if ans ** 3 != abs(x):
     print(str(x) + ' is not a perfect cube')
 else:
     if x < 0:
-        ans1 = -ans1
+        ans = -ans
     print('Cube root of ' + str(x) + ' is ' + str(ans))
