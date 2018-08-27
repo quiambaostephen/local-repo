6be6a5f20b34179efe620bbdb55e62d156b0ec34
Stephen Quiambao <quiambaostephen@gmail.com>
2018-08-27 13:54:50 +0800

    Initial Commit

---
+++ newton_raphson.py
@@ -0,0 +1,8 @@
+y = 24
+epsilon = 0.01
+guess = y / 2.0
+
+while abs(guess * guess - y) >= epsilon:
+    guess = guess - (((guess ** 2) - y) / (2 * guess))
+    print(guess)
+print('Square root of ' + str(y) + ' is about ' + str(guess))
