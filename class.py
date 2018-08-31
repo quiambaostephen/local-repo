0b223a1e142d3c3fc204927a9414d3d87c88d236
Stephen Quiambao <quiambaostephen@gmail.com>
2018-08-31 18:27:28 +0800

    Added new method

--- class.py
+++ class.py
@@ -1,13 +1,19 @@
 class Employee:
+    raise_amount = 1.04
+    num_of_emps = 0
+
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
-        self.email = first + last + '@company.com'
+        self.email = first + '.' + last + '@company.com'
+        Employee.num_of_emps += 1
 
     def fullName(self):
         return '{} {}'.format(self.first, self.last)
 
+    def applyRaise(self):
+        self.pay = int(self.pay * 1.04)
+
 
 emp1 = Employee('John', 'Doe', 5000)
-print(emp1)
-print(emp1.fullName())
+print(Employee.num_of_emps)
