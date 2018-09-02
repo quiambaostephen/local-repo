45a172ffbe9ca25aac0ce55cd263f712894e1027
Stephen Quiambao <quiambaostephen@gmail.com>
2018-09-02 22:52:22 +0800

    Added if else

--- iter_power.py
+++ iter_power.py
@@ -4,3 +4,12 @@ def iter_power(base, exp):
         exp: int >= 0
 
         returns int or float, base^exp
+    '''
+    x = base
+
+    if exp == 0:
+        return 1
+    else:
+        for i in range(1, exp):
+            base *= x
+        return base
