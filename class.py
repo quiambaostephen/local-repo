63239ccfdd82a3aadb96a387b9c5b940922a203b
Stephen Quiambao <quiambaostephen@gmail.com>
2018-09-04 17:01:12 +0800

    Added subclasses

--- class.py
+++ class.py
@@ -15,5 +15,43 @@ class Employee:
         self.pay = int(self.pay * self.raise_amount)
 
 
-emp1 = Employee('John', 'Doe', 5000)
-print(Employee.num_of_emps)
+ class Developer(Employee):
+    raise_amt = 1.10
+
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
+
+
+class Manager(Employee):
+    def __init__(self, first, last, pay, employees=None):
+        if employees is None:
+            employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+           self.employees.remove(emp)
+
+     def print_emps(self):
+        for emp in self.employees:
+            print('-->', self.fullname())
+
+
+dev_1 = Developer('John', 'Doe', 5000, 'Python')
+dev_2 = Developer('Jane', 'Doe', 6000, 'Java')
+
+mgr_1 = Manager('Sue', 'Smith', 9000, [dev1])
+
+print(mgr_1.email)
+
+mgr_1.add_emp(dev1)
+mgr_1.remove_emp(dev1)
+
+mgr_1.print_emps()
+
